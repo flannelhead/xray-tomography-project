2d1ecc039379e49b97a72fb4270ad5a25cc2b394
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-03-01 13:02:26 +0200

    morphological closing and masking

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -8,7 +8,7 @@ from skimage.viewer import CollectionViewer, ImageViewer
 
 SLICE_DIR = 'slices'
 SLICE_PATTERN = '*.tif'
-
+CLOSING_RADIUS = 2
 
 def load_uint8(f, **kwargs):
     return util.img_as_ubyte(io.imread(f), force_copy=True)
@@ -18,10 +18,14 @@ slice_collection = io.ImageCollection(path.join(SLICE_DIR, SLICE_PATTERN),
                                       load_func=load_uint8)
 
 vol_img = slice_collection.concatenate()
+
 otsu_threshold = filters.threshold_otsu(vol_img)
-
 img_binary = vol_img >= otsu_threshold
+img_binary = morphology.binary_closing(img_binary,
+                                       selem=morphology.ball(CLOSING_RADIUS))
 
-viewer = CollectionViewer(img_binary)
+img_masked = vol_img & util.img_as_ubyte(img_binary)
+
+viewer = CollectionViewer(img_masked)
 viewer.show()
 
